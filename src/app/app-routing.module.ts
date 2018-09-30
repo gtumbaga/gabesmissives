52b721ec2fbd5db67e6917582cef007cc4962f42
Gabriel Tumbaga <gtumbaga@Masamune.local>
2018-09-29 20:32:34 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageFrontComponent } from './page-front/page-front.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
+import { BlogpostComponent } from './blogpost/blogpost.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PageFrontComponent
+  },
+  {
+    path: 'about',
+    component: PageAboutComponent
+  },
+  {
+    path: 'contact',
+    component: PageContactComponent
+  },
+  {
+    path: 'blogs/:slug',
+    component: BlogpostComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainheaderComponent } from './mainheader/mainheader.component';
+import { BlogpostComponent } from './blogpost/blogpost.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { FooterComponent } from './footer/footer.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
+import { PageFrontComponent } from './page-front/page-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainheaderComponent,
+    BlogpostComponent,
+    FooterComponent,
+    PageAboutComponent,
+    PageContactComponent,
+    PageFrontComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
